27c7bff0a5f24e88bb2db3263e42873e62381ac9
Razz <razmikachikyan5@gmail.com>
2022-09-18 00:35:43 +0400

    done

---
+++ main.js
@@ -0,0 +1,4 @@
+debugger
+const div = document.querySelector("div");
+div.innerHTML = "";
+div.innerHTML = `<h1>Welcome</h1><h1>Example Domain</h1><p>This domain is for use in illustrative examples in documents. You may use thisdomain in literature without prior coordination or asking for permission.</p><p><a href="https://www.iana.org/domains/example">More information...</a></p>`
